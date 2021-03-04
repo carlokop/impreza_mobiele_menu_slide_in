3418562bf969d293bfd1eba597b5c6087ef57867
Carlo <ch.kop.ck@gmail.com>
2021-03-04 01:18:51 +0100

    Revert "site gebroken"
    This reverts commit 769187adbf92751f76898bdeeea1aaa5bf703db6.

--- wp-content/themes/ponck-child/templates/template-parts/filters.php
+++ wp-content/themes/ponck-child/templates/template-parts/filters.php
@@ -4,27 +4,27 @@
 *   Dit template wordt via shortcodes gebruik
 *   $post_type en $num_posts moet reeds worden geladen in de shortcode;
 */
-
+global $post;
 $categories = get_terms(array(
     'taxonomy' => $tax_type,
-    'hide_empty' => true,
+    'hide_empty' => false,
 ));
 
 ?>
-<div class="row-centered filter-options">
+<div class="row-centered">
     <span class='float-left intro'><?php echo $intro; ?></span>
     <ul class="filters">
         <li><a href="<?php echo get_bloginfo("url"); ?>/verhalen/" class="btn outline secondary <?php if (!is_category()) echo 'active'; ?>">Alle</a></li>
 
         <?php
 
-        $curr = get_queried_object();
+        $curr = get_the_category();
         foreach ($categories as $category) {
 
-            if ($category->term_id == $curr->term_id) $active = "active";
+            if ($category->term_id == $curr[0]->cat_ID) $active = "active";
             else $active = '';
 
-            echo '<li><a href="' . get_category_link($category) . '" class="btn outline secondary ' . $active . '">' . ucfirst($category->name) . '</a></li>';
+            echo '<li><a href="' . get_category_link($category) . '" class="btn outline secondary ' . $active . '">' . $category->name . '</a></li>';
         }
 
         ?>
