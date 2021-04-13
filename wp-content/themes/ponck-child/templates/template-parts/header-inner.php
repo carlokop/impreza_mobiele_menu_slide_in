0c802fb0a5c637210fd5300e6a794cc0800a5b49
Carlo <ch.kop.ck@gmail.com>
2021-04-13 13:59:27 +0200

    menu knop mobiel

--- wp-content/themes/ponck-child/templates/template-parts/header-inner.php
+++ wp-content/themes/ponck-child/templates/template-parts/header-inner.php
@@ -11,8 +11,6 @@
         <div class="animated-hamburger-icon">
             <span></span>
             <span></span>
-            <span></span>
-            <span></span>
         </div>
     </div>
 </div>
